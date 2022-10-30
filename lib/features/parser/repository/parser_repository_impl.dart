9f327f8fb3dd043a21bbebae5eff96ee4f802361
Ivan Igonkin <ivanivan120@gmail.com>
2022-10-30 18:27:11 +0300

    Start adding Parser.

---
+++ lib/features/parser/repository/parser_repository_impl.dart
@@ -0,0 +1,9 @@
+import 'package:data_visualizer/features/parser/repository/parser_repository.dart';
+
+class ParserRepositoryImpl implements ParserRepository {
+  @override
+  List<String> getTermNames() {
+    return [];
+  }
+
+}
